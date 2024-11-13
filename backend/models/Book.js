e65f2e522672e6f55ff6f22488fa5fe5db9f718b
huonghuongho <142366403+huonghuongho@users.noreply.github.com>
2024-11-13 17:26:07 -0500

    Update Book.js
    update

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 // Define the schema for the Book model
 const BookSchema = new mongoose.Schema({
-  book_id: { type: Number, required: true, unique: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   isbn: { type: String, required: true, unique: true },
   publisher: { type: String },
-  year_published: { type: String },
+  year_published: { type: Number },
   category: { type: String },
-  quantity: { type: String }
+  quantity: { type: Number }
 });
 
 // Export the model using the schema
