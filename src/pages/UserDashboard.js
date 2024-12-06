25740b85229cb5aee982777c45030a1d95d0270e
SXY200010 <156553242+SXY200010@users.noreply.github.com>
2024-12-06 01:20:37 -0500

    Update style

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -114,7 +114,7 @@ function UserDashboard({ username, email }) {
 
   return (
     <div>
-      <h2>Good Evening! {userInfo.username}!</h2>
+      <h2 class='helloUser'>Good Evening! {userInfo.username}!</h2>
       <div className="user-dashboard">
         <div className="sidebar">
           <ul>
